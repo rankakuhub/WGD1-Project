4a1725b55031217eb53093851b740e2646520a33
DarrenBoyd00 <darren.boyd@hotmail.co.uk>
2021-01-06 13:12:53 +0000

    character

--- public/js/Game.js
+++ public/js/Game.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -13,26 +13,60 @@ var config = {
     scene: {
         preload: preload,
         create: create,
+        update: update,
     }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
 
     this.load.image('tiles', '../assets/tilesets/level1__tilesheet.png');
     this.load.image('ground', '../assets/images/Level1_Ground.png');
     this.load.image('walls', '../assets/images/Level1_Walls.png');
+    this.load.image('player', '../assets/images/PlayerCharacter.png');
 
     this.load.json('map', '../assets/tilemaps/Level1.json');
 
 }
 
+//var inputCursor;
+let playerSprite
 function create() {
 
-    const backgroundImage = this.add.image(0,0, 'ground').setOrigin(0,0);
-    const Image = this.add.image(0,0, 'walls').setOrigin(0,0);
+    this.add.image(0,0, 'ground').setOrigin(0,0);
+    this.add.image(0,0, 'walls').setOrigin(0,0);
+    playerSprite = this.physics.add.sprite(130, 445, 'player');
+    playerSprite.setScale(0.3);
 
+    this.keys = this.input.keyboard.createCursorKeys();
+    this.cameras.main.startFollow(playerSprite);
+    this.cameras.main.zoom = 3;
+   // this.input = inputCursor;
+
+}
+
+function update(){
+   // let angle=Phaser.Math.Angle.Between(playerSprite.x,playerSprite.y,inputCursor.x,inputCursor.y);
+
+    this.keys = this.input.keyboard.addKeys(
+        {up:Phaser.Input.Keyboard.KeyCodes.W,
+            down:Phaser.Input.Keyboard.KeyCodes.S,
+            left:Phaser.Input.Keyboard.KeyCodes.A,
+            right:Phaser.Input.Keyboard.KeyCodes.D});
+
+    if (this.keys.left.isDown) {
+        playerSprite.x -= 1.5;
+    }
+    if (this.keys.right.isDown) {
+        playerSprite.x += 1.5;
+    }
+    if (this.keys.up.isDown) {
+        playerSprite.y -= 1.5;
+    }
+    if (this.keys.down.isDown) {
+        playerSprite.y += 1.5;
+    }
 
 
 }
